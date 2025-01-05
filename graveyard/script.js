54edfbf237f07c2eacf8e67c5372ca341e5115cc
purehazedev <151788370+purehazedev@users.noreply.github.com>
2025-01-05 15:20:55 +0200

    Add files via upload

---
+++ graveyard/script.js
@@ -0,0 +1,6 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Simulate form submission
+    alert("Thank you for contacting us! We will get back to you soon.");
+});
